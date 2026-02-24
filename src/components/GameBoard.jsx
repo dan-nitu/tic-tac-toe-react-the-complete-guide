8b0241e22caaa13c1a396f0a2b379fac38b4ab9d
Voodoo30fps <voodoo30fps@gmail.com>
2026-02-24 21:45:30 +0200

    added game log. removed the state from game board

--- src/components/GameBoard.jsx
+++ src/components/GameBoard.jsx
@@ -1,26 +1,33 @@
-import { useState } from 'react';
-
 const initialGameBoard = [
   [null, null, null],
   [null, null, null],
   [null, null, null],
 ];
 
-export default function GameBoard({ onSelectSquare, activePlayersSymbol }) {
-  const [gameBoard, setGameBoard] = useState(initialGameBoard);
+export default function GameBoard({ onSelectSquare, turns }) {
+  let gameBoard = initialGameBoard;
 
-  function handleSelectSquare(rowIndex, colIndex) {
-    setGameBoard((prevGameBoard) => {
-      const updatedBoard = [
-        ...prevGameBoard.map((innerArray) => [...innerArray]),
-      ];
-      updatedBoard[rowIndex][colIndex] = activePlayersSymbol;
-      return updatedBoard;
-      // bets practice on how to update state that is an array or object. always create a new copy of the state and update the copy, rather than mutating the original state directly. this ensures that React can properly detect changes and re-render components as needed.
-    });
+  for (const turn of turns) {
+    const { square, player } = turn;
+    const { row, col } = square;
 
-    onSelectSquare();
+    gameBoard[row][col] = player;
   }
+
+  // const [gameBoard, setGameBoard] = useState(initialGameBoard);
+
+  // function handleSelectSquare(rowIndex, colIndex) {
+  //   setGameBoard((prevGameBoard) => {
+  //     const updatedBoard = [
+  //       ...prevGameBoard.map((innerArray) => [...innerArray]),
+  //     ];
+  //     updatedBoard[rowIndex][colIndex] = activePlayersSymbol;
+  //     return updatedBoard;
+  //     // bets practice on how to update state that is an array or object. always create a new copy of the state and update the copy, rather than mutating the original state directly. this ensures that React can properly detect changes and re-render components as needed.
+  //   });
+
+  //   onSelectSquare();
+  // }
 
   return (
     <ol id='game-board'>
@@ -29,7 +36,7 @@ export default function GameBoard({ onSelectSquare, activePlayersSymbol }) {
           <ol>
             {row.map((playerSymbol, colIndex) => (
               <li key={colIndex}>
-                <button onClick={() => handleSelectSquare(rowIndex, colIndex)}>
+                <button onClick={() => onSelectSquare(rowIndex, colIndex)}>
                   {playerSymbol}
                 </button>
               </li>
